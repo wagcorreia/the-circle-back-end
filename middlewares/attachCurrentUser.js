cd23ef3ad96dbc6a77317720cec2a6a9ddff8ad7
Gabriel Escame <gabriel.escame@gmail.com>
2021-07-29 10:47:49 -0300

    feat: creating  user received messages

--- middlewares/attachCurrentUser.js
+++ middlewares/attachCurrentUser.js
@@ -8,7 +8,7 @@ module.exports = async (req, res, next) => {
     const user = await UserModel.findOne(
       { _id: loggedInUser._id },
       { passwordHash: 0, __v: 0 } // Excluindo o hash da senha da resposta que vai pro servidor, por segurança
-    );
+    ).populate("postID");
 
     if (!user) {
       // 400 significa Bad Request
--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,125 +1,143 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcryptjs')
-const saltRounds = 10
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 
-const UserModel = require('../models/User.model')
-const MessageModel = require('../models/Message.model')
+const UserModel = require("../models/User.model");
+const MessageModel = require("../models/Message.model");
 
-const isAuthenticated = require('../middlewares/isAuthenticated')
-const attachCurrentUser = require('../middlewares/attachCurrentUser')
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const attachCurrentUser = require("../middlewares/attachCurrentUser");
 
 //criar nova mensagem
 router.post(
-  '/message',
+  "/message",
   isAuthenticated,
   attachCurrentUser,
   async (req, res, next) => {
     try {
-      const { messagebody, userId_received } = req.body
+      const { messagebody, userId_received } = req.body;
 
-      const loggedInUser = req.currentUser
+      const loggedInUser = req.currentUser;
       //criando mensagem
       const newMessage = await MessageModel.create({
         userId_sending: loggedInUser._id,
+        userName_sending: loggedInUser.name,
         messagebody: messagebody,
         userId_received,
-      })
-      console.log(newMessage)
+      });
+      console.log(newMessage);
       //procurando id do usuário para atualizar mensagem nele
       const updateddeletedMessage = await UserModel.findOneAndUpdate(
         { _id: userId_received },
-        { $push: { messengerID: newMessage._id } },
-      ) //salvando mensagem para array do usuário
+        { $push: { messengerID: newMessage._id } }
+      ); //salvando mensagem para array do usuário
 
       if (updateddeletedMessage) {
-        return res.status(200).json(updateddeletedMessage)
+        return res.status(200).json(updateddeletedMessage);
       }
 
-      return res.status(201).json(updateddeletedMessage)
+      return res.status(201).json(updateddeletedMessage);
     } catch (err) {
-      next(err)
+      next(err);
     }
-  },
-)
+  }
+);
 
 // Listar todas mensagens
 router.get(
-  '/allmessages',
+  "/allmessages",
   isAuthenticated,
   attachCurrentUser,
   async (req, res, next) => {
     try {
-      const allMessage = await MessageModel.find()
+      const allMessage = await MessageModel.find();
 
-      return res.status(200).json(allMessage)
+      return res.status(200).json(allMessage);
     } catch (err) {
-      next(err)
+      next(err);
     }
-  },
-)
+  }
+);
 
 // busca uma mensagem especifica
 router.get(
-  '/message/:id',
+  "/message/:id",
   isAuthenticated,
   attachCurrentUser,
   async (req, res, next) => {
     try {
-      const { id } = req.params
+      const { id } = req.params;
 
       const message = await MessageModel.findOne({
         _id: id,
-      })
+      });
 
-      return res.status(200).json(message)
+      return res.status(200).json(message);
     } catch (err) {
-      next(err)
+      next(err);
     }
-  },
-)
+  }
+);
 
 //deletando mensagem
 router.delete(
-  '/message/:id',
+  "/message/:id",
   isAuthenticated,
   attachCurrentUser,
   async (req, res, next) => {
     try {
-      const { id } = req.params
+      const { id } = req.params;
       //buscar a menssagem
-      const messageReceived = await MessageModel.findOne({ _id: id })
+      const messageReceived = await MessageModel.findOne({ _id: id });
 
       console.log(
-        String(req.currentUser._id) === String(messageReceived.userId_sending),
-      )
+        String(req.currentUser._id) === String(messageReceived.userId_sending)
+      );
 
       if (
         String(req.currentUser._id) !== String(messageReceived.userId_sending)
       ) {
-        return res.status(401).json({})
+        return res.status(401).json({});
       }
 
-      const { userId_received } = messageReceived
+      const { userId_received } = messageReceived;
       // Deletar menssagem do banco
       const message = await MessageModel.deleteOne({
         _id: id,
-      })
+      });
       if (message.n > 0) {
         const updatedeteledmessage = await UserModel.findOneAndUpdate(
           { _id: messageReceived.userId_received },
           { $pull: { messengerID: id } },
-          { new: true },
-        )
+          { new: true }
+        );
         if (updatedeteledmessage) {
-          return res.status(200).json({})
+          return res.status(200).json({});
         }
-        return res.status(404).json({ error: 'Messagem não encontrada' })
+        return res.status(404).json({ error: "Messagem não encontrada" });
       }
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
-)
 
-module.exports = router
+  router.get(
+    "/user-received-messages",
+    isAuthenticated,
+    attachCurrentUser,
+    async (req, res, next) => {
+      try {
+        const receivedMessage = await MessageModel.find({
+          userId_received: req.currentUser,
+        });
+        console.log("receivedMessage", { receivedMessage });
+        return res.status(200).json(receivedMessage);
+      } catch (err) {
+        next(err);
+      }
+    }
+  )
+);
+
+module.exports = router;
